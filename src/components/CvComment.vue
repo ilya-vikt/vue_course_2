2c990f302be106be4840e5b7eb6a08a5622e84a0
Ilya Viktorov <ilya-vikt@yandex.ru>
2022-09-09 18:07:17 +0300

    add the One Comment component

---
+++ src/components/CvComment.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+      <strong>{{ author }}</strong>
+    </p>
+    <small>{{ text }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    author: String,
+    text: String,
+  },
+};
+</script>
--- src/components/CvCommentList.vue
+++ src/components/CvCommentList.vue
@@ -3,15 +3,16 @@
     <h2>Комментарии</h2>
     <ul class="list">
       <li class="list-item">
-        <div>
-          <p><strong>test@microsoft.com</strong></p>
-          <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
-        </div>
+        <cv-comment author="ttt" text="text"></cv-comment>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import CvComment from './CvComment.vue';
+
+export default {
+  components: { CvComment },
+};
 </script>
